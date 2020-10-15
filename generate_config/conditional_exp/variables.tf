aac35ac17d5e0afb6af140f2b48210f55c73ebf1
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-10-14 21:12:23 -0500

    Add files via upload

---
+++ generate_config/conditional_exp/variables.tf
@@ -0,0 +1,5 @@
+variable "image" { default = "ubuntu-os-cloud/ubuntu-1604-lts"}
+variable "machine_type" { default = "n1-standard-1"}
+variable "environment" {default = "test"}
+variable "machine_type_dev" {default = "n1-standard-4"}
+variable "zone" { default = "us-central1-a"}
