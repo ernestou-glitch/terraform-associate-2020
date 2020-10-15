c454cda286f71e7fefd8160d7d8b0b7fcb15540f
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-10-14 21:13:01 -0500

    Create main.tf

---
+++ generate_config/count/main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_instance" "default" {
+#count = 4
+count = length(var.instance_name)
+name         = var.instance_name[count.index]
+machine_type = var.machine_type
+zone         = var.instance_zone
+boot_disk {
+    initialize_params {
+        image = var.instance_image
+    }
+}
+network_interface {
+    network = var.vpc_network
+    access_config {
+    }
+}
+}
+output "first_instance_name" { value = google_compute_instance.default.0.name}
+output "all_instance_names" { value = google_compute_instance.default.*.name}
