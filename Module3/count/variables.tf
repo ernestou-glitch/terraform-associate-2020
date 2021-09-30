689b69393d3665881c3354f708ad946b09f3cc4d
Ernesto Andres Urquijo Gonzalez <61810527+ernestou-glitch@users.noreply.github.com>
2021-09-30 10:06:51 -0500

    Add files via upload

---
+++ Module3/count/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_name" {
+    type = list(string)
+}
+
+variable "machine_type" {
+    type = string
+}
+
+variable "instance_zone" {
+    type = string
+}
+
+variable "instance_image" {
+    type = string
+}
+
+variable "vpc_network" {
+    type = string
+}
