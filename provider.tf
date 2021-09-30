42ce8d2b9d0f6a985bc469d63da714cfaeb80a48
Ernesto Andres Urquijo Gonzalez <61810527+ernestou-glitch@users.noreply.github.com>
2021-09-30 10:01:42 -0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,16 @@
+provider "google" {
+
+}
+
+/*
+provider "google" {
+    project = "academic-oasis-312313"
+}
+
+#Best Practice to include provider version
+provider "google" {
+    project = "academic-oasis-312313"
+    version = ">= 3.42"
+}
+
+*/
