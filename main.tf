220cfbebc5f7f38078be68bc944734dc2fd03b20
Ernesto Andres Urquijo Gonzalez <61810527+ernestou-glitch@users.noreply.github.com>
2021-09-30 10:12:53 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
