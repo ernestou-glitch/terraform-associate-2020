76ab967cda32ce4bc5c467bb4984da4136f6c9ac
Ernesto Andres Urquijo Gonzalez <61810527+ernestou-glitch@users.noreply.github.com>
2021-09-30 10:17:59 -0500

    Add files via upload

---
+++ Module4/workspaces/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "default" {
+    name = var.gce_name
+    machine_type = "f1-micro"
+    zone = var.gce_zone
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-9"
+            size = var.gce_disk_size
+        }
+    }
+
+    network_interface {
+        network = "default"
+    }
+}
---
+++ Module4/workspaces/variables.tf
@@ -0,0 +1,13 @@
+variable "gce_name" {
+
+}
+
+variable "gce_zone" {
+    type = string
+    default = "us-central1-b"
+}
+
+variable "gce_disk_size" {
+    type = number
+    description = "Size for the primary disk of the VM"
+}
