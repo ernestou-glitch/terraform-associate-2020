653be6852fa3b104ebb3d8fc503b545c2896a8c7
Ernesto Andres Urquijo Gonzalez <61810527+ernestou-glitch@users.noreply.github.com>
2021-09-30 10:13:57 -0500

    Add files via upload

---
+++ Module4/gcs/variables.tf
@@ -0,0 +1,13 @@
+#Variables for provider.tf file
+variable "project_id" {
+}
+
+#Variables for main.tf
+variable "bucket_name" { 
+}
+
+variable "bucket_location" {
+}
+
+variable "bucket_class" {
+}
